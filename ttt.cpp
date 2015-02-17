6f0248bbce62b6e9c1e9d37fc09c8632db532d87
Garoe <garoedp@gmail.com>
2015-02-17 23:29:40 +0000

    Added basic code for a greedy play

--- ttt.cpp
+++ ttt.cpp
@@ -19,6 +19,16 @@ std::ostream & operator<<(std::ostream & os, const Node & nd) {
 	return os;
 }
 
+std::ostream & operator<<(std::ostream & os, const Board & board) {
+	for (unsigned int i = 0; i < 3; i++) {
+		for (unsigned int j = 0; j < 3; j++) {
+			os << board.getBoard(i, j);
+		}
+		os << std::endl;
+	}
+	return os;
+}
+
 char switchTurn(char turn) {
 	if (turn == 'x') {
 		return 'o';
@@ -29,6 +39,7 @@ char switchTurn(char turn) {
 void buildNode(tree<Node>& tr, TreeIt nodeIt, char turn, unsigned int nextI,
 		unsigned int nextJ) {
 	nodeIt->setBoard(nextI, nextJ, turn);
+	nodeIt->setA(nextI, nextJ);
 
 	nodeIt->computeFinalState(turn);
 
@@ -61,6 +72,7 @@ void buildTree(tree<Node>& tr, char turn) {
 		for (unsigned int j = 0; j < 3; j++) {
 			nodeIt = tr.insert(root, emptyNode);
 			nodeIt->setBoard(i, j, turn);
+			nodeIt->setA(i, j);
 			for (unsigned int k = 0; k < 3; k++) {
 				for (unsigned int l = 0; l < 3; l++) {
 					if (nodeIt->getBoard(k, l) == 'e') {
@@ -75,16 +87,71 @@ void buildTree(tree<Node>& tr, char turn) {
 
 int main(int, char **) {
 	tree<Node> tr;
-	const TreeIt root = tr.begin();
+	//const TreeIt root = tr.begin();
+	//tree<Node>::sibling_iterator nextSib;
+	//TreeIt nextNd;
+
+	tree_node_<Node> * currentNode;
+	tree<Node>::sibling_iterator nextSib, endSib, nextNode;
+
 	buildTree(tr, 'x');
 	cout << "done building size " << tr.size() << endl;
 
-	int maxGames = 10;
+	char turn = 'x';
+	unsigned int maxGames = 10;
+	float maxV = -100;
+	const tree<Node>::sibling_iterator firstNode = tr.begin().node;
 	Board board;
 	for (unsigned int i = 1; i <= maxGames; i++) {
-		//while (!board.isFinalState()) {
+		currentNode = firstNode.node;
 
-		//}
+		maxV = -100;
+
+		// Get next move with max V
+		nextSib = tr.begin().node;
+		endSib = tr.end().node;
+		nextNode = nextSib;
+		maxV = nextNode->getV();
+		while (++nextSib != endSib) {
+			if (nextSib->getV() > maxV) {
+				maxV = nextSib->getV();
+				nextNode = nextSib;
+			}
+		}
+
+		// Make the play
+		board.setBoard(nextNode->getA(), turn);
+
+		cout << board << endl;
+
+		turn = switchTurn(turn);
+		currentNode = nextNode.node;
+
+		while (!board.isFinalState()) {
+			maxV = -100;
+
+			// Get next move with max V
+			nextSib = currentNode->first_child;
+			endSib = currentNode->last_child;
+			nextNode = nextSib;
+			maxV = nextNode->getV();
+			while (++nextSib != endSib) {
+				if (nextSib->getV() > maxV) {
+					maxV = nextSib->getV();
+					nextNode = nextSib;
+				}
+			}
+
+			// Make the play
+			board.setBoard(nextNode->getA(), turn);
+
+			cout << board << endl;
+
+			turn = switchTurn(turn);
+			currentNode = nextNode.node;
+		}
+
+		return 0;
 	}
 
 }
