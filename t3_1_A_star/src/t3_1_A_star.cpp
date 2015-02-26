ba1b5f2c14dac64d2a99d5debd8edbe9edbdcb33
Garoe <garoedp@gmail.com>
2015-02-26 08:22:46 +0000

    Added main file for task3_1 A* search

---
+++ t3_1_A_star/src/t3_1_A_star.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : t3_1_A_star.cpp
+// Author      : Garoe
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
