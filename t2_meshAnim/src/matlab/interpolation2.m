e727e09bdc6589ce574284c5537eac497754a397
Garoe <garoedp@gmail.com>
2015-03-13 08:21:38 +0000

    Fixed errors on first triangle calculation in as rigig as possible interpolation

--- t2_meshAnim/src/matlab/interpolation2.m
+++ t2_meshAnim/src/matlab/interpolation2.m
@@ -7,6 +7,12 @@ save_path = '~/workspaces/matlab/cag2/data/interpolateResult/interpolated_t_';
 if ~(exist('sourceShapeObj', 'var') && exist('targetShapeObj', 'var'))
     sourceShapeObj = read_wobj('~/workspaces/matlab/cag2/data/horse_source.obj');
     targetShapeObj = read_wobj('~/workspaces/matlab/cag2/data/horse_target.obj');
+    toSaveObj.objects = sourceShapeObj.objects;
+    toSaveObj.material = sourceShapeObj.material;
+    toSaveObj.vertices = sourceShapeObj.vertices;
+    toSaveObj.vertices_point = sourceShapeObj.vertices_point;
+    toSaveObj.vertices_normal = sourceShapeObj.vertices_normal;
+    toSaveObj.vertices_texture = sourceShapeObj.vertices_texture;
 end
 
 T = sourceShapeObj.objects(1,5).data.vertices;
@@ -31,23 +37,27 @@ bt = cell(size(T,1),1);
 
 % First triangle. Ap + l = q.
 zero3 = [0,0,0];
-Ptr = [p(1,:), zero3, zero3, 1 0 0;
-    zero3, p(1,:), zero3, 0 1 0;
-    zero3, zero3, p(1,:), 0 0 1;
-    p(2,:), zero3, zero3, 1 0 0;
-    zero3, p(2,:), zero3, 0 1 0;
-    zero3, zero3, p(2,:), 0 0 1;
-    p(3,:), zero3, zero3, 1 0 0;
-    zero3, p(3,:), zero3, 0 1 0;
-    zero3, zero3, p(3,:), 0 0 1];
 
-Qtr = [q(1,:), q(2,:), q(3,:)]';
+i = 1;
 
-% A and l.
-% Doing the inverse like this yields the same result for AFulltr if we do
-% inP{1} * Qtr
+vertex1 = T(i,1);
+vertex2 = T(i,2);
+vertex3 = T(i,3);
+
+Ptr = [p(vertex1,:), zero3, zero3, 1 0 0;
+    zero3, p(vertex1,:), zero3, 0 1 0;
+    zero3, zero3, p(vertex1,:), 0 0 1;
+    p(vertex2,:), zero3, zero3, 1 0 0;
+    zero3, p(vertex2,:), zero3, 0 1 0;
+    zero3, zero3, p(vertex2,:), 0 0 1;
+    p(vertex3,:), zero3, zero3, 1 0 0;
+    zero3, p(vertex3,:), zero3, 0 1 0;
+    zero3, zero3, p(vertex3,:), 0 0 1];
+
+Qtr = [q(vertex1,:), q(vertex2,:), q(vertex3,:)]';
+
+% inP = inv(Ptr);
 inP = Ptr\eye(size(Ptr));
-% Save only the A values of the inverse
 inP = inP(1:9, 1:9);
 
 Afulltr = inP * Qtr;
@@ -166,9 +176,9 @@ x = zeros(size(p,1)-1, 3);
 %% Main loop.
 for t = 0:0.1:1
     disp('Computing next frame');
-    v1x = (1-t)*p(1,1) + t*q(1,1);
-    v1y = (1-t)*p(1,2) + t*q(1,2);
-    v1z = (1-t)*p(1,3) + t*q(1,3);
+    v1x = (1-t)*p(T(1,1),1) + t*q(T(1,1),1);
+    v1y = (1-t)*p(T(1,1),2) + t*q(T(1,1),2);
+    v1z = (1-t)*p(T(1,1),3) + t*q(T(1,1),3);
     
     A = ((1-t)*eye(3) + t*Rgamma{1}) * ((1-t)*eye(3) + t*S{1});
     
@@ -216,20 +226,20 @@ for t = 0:0.1:1
     k = 2;
     for j = 1:3:size(u,1)
         x(k,:) = [u(j),u(j+1),u(j+2)];
-        sourceShapeObj.vertices(k-1,:) = [u(j),u(j+1),u(j+2)];
+        toSaveObj.vertices(k-1,:) = x(k,:);
         k = k + 1;
     end
     
-    write_wobj(sourceShapeObj, strcat(save_path, num2str(t), '.obj'));
+    write_wobj(toSaveObj, strcat(save_path, num2str(t), '.obj'));
     
     fprintf('Displaying t = %f\n',t);
     % Plot.
     figure(1);
     hold on;
     trisurf(T(2:end,:), p(:, 1), p(:,2), p(:,3), ones(1,size(p,1)));
-    trisurf(T(2:end,:), q(:, 1), q(:,2), q(:,3), ones(1,size(p,1))+1);
-    trisurf(T(2:end,:), x(:, 1), x(:,2), x(:,3), ones(1,size(x,1))+2);
-    view(90,1);
+    trisurf(T(2:end,:), x(:, 1) + 30, x(:,2), x(:,3), ones(1,size(x,1))+2);
+    trisurf(T(2:end,:), q(:, 1) + 60, q(:,2), q(:,3), ones(1,size(p,1))+1);
+    view(50,30);
     pause('on');
     pause;
     clf(figure(1));
