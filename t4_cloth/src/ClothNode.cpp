e7dcedb4b45aff3daa5c354089312591a3a20cf6
Garoe <garoedp@gmail.com>
2015-03-27 14:28:07 +0000

    Fixed error in previous commit

--- t4_cloth/src/ClothNode.cpp
+++ t4_cloth/src/ClothNode.cpp
@@ -286,10 +286,10 @@ void ClothNode::addAttributes() {
 }
 
 void ClothNode::makeInput(MFnNumericAttribute &attr) {
-	CHECK_MSTATUS(attr.setKeyable(false));
-	CHECK_MSTATUS(attr.setStorable(false));
+	CHECK_MSTATUS(attr.setKeyable(true));
+	CHECK_MSTATUS(attr.setStorable(true));
 	CHECK_MSTATUS(attr.setReadable(true));
-	CHECK_MSTATUS(attr.setWritable(false));
+	CHECK_MSTATUS(attr.setWritable(true));
 }
 
 void ClothNode::makeOutput(MFnNumericAttribute &attr) {
