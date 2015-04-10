95105797d3cf3d205a010f7b03474cad4924cca0
Garoe <garoedp@gmail.com>
2015-04-10 16:12:03 +0100

    Deleted log file for rendering script

--- t4_cloth/code/render.sh
+++ t4_cloth/code/render.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 > /home/gdp24/maya/projects/cag2_cloth/renderlog.txt
-Render -rd /home/gdp24/maya/projects/cag2_cloth/images -im outputim -of jpg -log /home/gdp24/maya/projects/cag2_cloth/renderlog.txt /home/gdp24/workspaces/github/cag2/t4_cloth/scenes/shaderTestGlobalIlum.ma
+Render -rd /home/gdp24/maya/projects/cag2_cloth/images -im outputim -of jpg /home/gdp24/workspaces/github/cag2/t4_cloth/scenes/shaderTestGlobalIlum.ma
