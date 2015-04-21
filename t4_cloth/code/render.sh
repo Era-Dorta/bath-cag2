36f9375b08df9e65f141e759981084552fe58bc5
Garoe <garoedp@gmail.com>
2015-04-21 15:05:14 +0100

    Using new scene in render.sh for cloth

--- t4_cloth/code/render.sh
+++ t4_cloth/code/render.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-> /home/gdp24/maya/projects/cag2_cloth/renderlog.txt
-Render -rd /home/gdp24/maya/projects/cag2_cloth/images -im outputim -of jpg /home/gdp24/workspaces/github/cag2/t4_cloth/scenes/shaderTestGlobalIlum.ma
+#> /home/gdp24/maya/projects/cag2_cloth/renderlog.txt
+Render -rd /home/gdp24/maya/projects/cag2_cloth/images -im outputim -of jpg /home/gdp24/workspaces/github/cag2/t4_cloth/scenes/shaderTest.ma
+#Render -rd /home/gdp24/maya/projects/cag2_cloth/images -im outputim -of jpg /home/gdp24/workspaces/github/cag2/t4_cloth/scenes/shaderTestGlobalIlum.ma
