82b6fa844db724aaf312fdf573a57b1e1ab80434
Garoe <garoedp@gmail.com>
2015-04-24 13:53:39 +0100

    Cloth node, added mental ray matrix comments and basic coordinate transform

--- t4_cloth/code/src/cloth_node.cpp
+++ t4_cloth/code/src/cloth_node.cpp
@@ -19,6 +19,16 @@ extern "C" DLLEXPORT int cloth_node_version(void) {
 	return (1);
 }
 
+const static miScalar eta = 1.46; // From the first measurements in the paper
+const static miScalar k_d = 0.3;
+const static miScalar gamma_s = 12;
+const static miScalar gamma_v = 24;
+const static miScalar a = 0.33;
+const static miVector A = { 0.2 * 0.3, 0.8 * 0.3, 0.3 };
+const static miScalar a1 = 0.5;
+const static miScalar a2 = 0.5;
+const static miScalar air_eta = 1;
+const static miScalar ior_in = 1;
 std::ostream& operator<<(std::ostream& os, const miVector& v) {
 	os << v.x << ", " << v.y << ", " << v.z;
 	return os;
@@ -62,7 +72,7 @@ extern "C" DLLEXPORT miBoolean cloth_node(miColor *result, miState *state,
 	diff = mi_eval_color(&paras->diffuse);
 	m = *mi_eval_integer(&paras->mode);
 
-	*result = *mi_eval_color(&paras->ambience); /* ambient term */
+	//*result = *mi_eval_color(&paras->ambience); /* ambient term */
 
 	n_l = *mi_eval_integer(&paras->n_light);
 	i_l = *mi_eval_integer(&paras->i_light);
@@ -98,6 +108,70 @@ extern "C" DLLEXPORT miBoolean cloth_node(miColor *result, miState *state,
 			}
 		}
 
+	// w -> omega, t -> theta
+	const miVector &w_i = state->dir;
+	miVector w_r;
+	mi_reflection_dir_diffuse(&w_r, state);
+	const miVector &n = state->normal;
+	const miVector &p = state->point;
+
+	// Get a new point in the triangle
+	miVector new_p = { 1, 0, 0 };
+	new_p.z = (-n.x * (new_p.x - p.x) - n.y * (new_p.y - p.y)) / n.z + p.z;
+	// Get vector from intersection to new point and normalize
+	miVector t;
+	mi_vector_sub(&t, &new_p, &p);
+	mi_vector_normalize_d(&t);
+	miVector s;
+	mi_vector_prod(&s, &n, &t);
+
+	// Build matrix that transform from current coordinate system to normalised
+	// one, where t is [1,0,0], n is [0,1,0] and s is [0,0,1]
+	miMatrix trans_to_p, rot_to_axis, trans_to_axis;
+
+	mi_matrix_ident(rot_to_axis);
+	rot_to_axis[0] = t.x;
+	rot_to_axis[1] = t.y;
+	rot_to_axis[2] = t.z;
+	rot_to_axis[4] = n.x;
+	rot_to_axis[5] = n.y;
+	rot_to_axis[6] = n.z;
+	rot_to_axis[8] = s.x;
+	rot_to_axis[9] = s.y;
+	rot_to_axis[10] = s.z;
+
+	mi_matrix_ident(trans_to_p);
+	trans_to_p[12] = -p.x;
+	trans_to_p[13] = -p.y;
+	trans_to_p[14] = -p.z;
+
+	mi_matrix_prod(trans_to_axis, trans_to_p, rot_to_axis);
+
+	/* Vectors are in columnwise
+	 * Matrices in mental ray are a row of 16 values, using the convention
+	 * r = v * M, where the translation component in the matrix in the last
+	 * column, and they are store columnwise
+	 * 0 4  8 12
+	 * 1 5  9 13
+	 * 2 6 10 14
+	 * 3 7 11 15
+	 * Rotations are counterclockwise, and the functions angles are in
+	 * radians */
+	miMatrix rot_m;
+	//mi_matrix_rotate(rot_m, 0, M_PI / 5, 0);
+	mi_matrix_ident(rot_m);
+	rot_m[12] = 1;
+	rot_m[13] = 2;
+	rot_m[14] = 3;
+	miVector resv, tv = { 1, 1, 1 };
+	mi_point_transform(&resv, &tv, rot_m);
+	//rotation[1] =
+
+	miScalar cos_t_i = fabs(mi_vector_dot(&n, &w_i));
+	miScalar t_i = acos(cos_t_i);
+
+	miScalar cos_t_r = fabs(mi_vector_dot(&n, &w_r));
+	miScalar t_r = acos(cos_t_r);
 	/* Compute indirect illumination */
 	miColor irrad;
 	mi_compute_avg_radiance(&color, state, 'f', NULL);
